1e131c3a8d9135777a13a02fe9f5fdd9d0660666
alper111 <ahmetoglu.alper@gmail.com>
2022-04-19 15:06:37 +0300

    deepsym dataset module

---
+++ dataset.py
@@ -0,0 +1,20 @@
+import os
+
+import torch
+
+
+class StateActionDataset(torch.utils.data.Dataset):
+    def __init__(self, path):
+        self.state = torch.load(os.path.join(path, "state.pt"))
+        self.action = torch.load(os.path.join(path, "action.pt"))
+
+    def __len__(self):
+        return len(self.state)-1
+
+    def __getitem__(self, idx):
+        sample = {}
+        sample["state"] = self.state[idx] / 255.0
+        sample["state_n"] = self.state[idx+1] / 255.0
+        sample["action"] = self.action[idx].float()
+        sample["effect"] = sample["state_n"] - sample["state"]
+        return sample
