9e959e6be4784a41c05afc10b0ee8ebd594da44f
alper111 <ahmetoglu.alper@gmail.com>
2022-04-29 09:43:17 +0300

    add state/action/effect dataset class

--- dataset.py
+++ dataset.py
@@ -18,3 +18,20 @@ class StateActionDataset(torch.utils.data.Dataset):
         sample["action"] = self.action[idx].float()
         sample["effect"] = sample["state_n"] - sample["state"]
         return sample
+
+
+class StateActionEffectDataset(torch.utils.data.Dataset):
+    def __init__(self, path):
+        self.state = torch.load(os.path.join(path, "state.pt"))
+        self.action = torch.load(os.path.join(path, "action.pt"))
+        self.effect = torch.load(os.path.join(path, "effect.pt"))
+
+    def __len__(self):
+        return len(self.state)
+
+    def __getitem__(self, idx):
+        sample = {}
+        sample["state"] = self.state[idx] / 255.0
+        sample["action"] = self.action[idx].float()
+        sample["effect"] = self.effect[idx] / 255.0
+        return sample
