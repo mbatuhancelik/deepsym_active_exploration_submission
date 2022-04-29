30eb54aa02cc413abfab225d54f03cc624265283
alper111 <ahmetoglu.alper@gmail.com>
2022-04-29 09:43:56 +0300

    parameterize train script

--- train.py
+++ train.py
@@ -5,56 +5,75 @@ import subprocess
 
 import torch
 
-from models import DeepSymbolGenerator
-from dataset import StateActionDataset
 import blocks
+from models import DeepSymbolGenerator
+from dataset import StateActionDataset, StateActionEffectDataset
+from utils import get_parameter_count
 
 parser = argparse.ArgumentParser("Train DeepSym.")
 parser.add_argument("-s", help="save folder", type=str, required=True)
+parser.add_argument("-d", help="data folder", type=str, required=True)
+parser.add_argument("-dt", help="dataset type. StateAction (0) or StateActionEffect (1)", type=int, required=True)
+parser.add_argument("-state_bits", help="state bits", type=int, required=True)
+parser.add_argument("-action_bits", help="action bits", type=int, required=True)
+parser.add_argument("-lr", help="learning rate", type=float, required=True)
+parser.add_argument("-e", help="num epochs", type=int, required=True)
+parser.add_argument("-bs", help="batch size", type=int, required=True)
+parser.add_argument("-bn", help="batch norm. True (1) or False (0)", type=int, required=True)
 args = parser.parse_args()
+
 
 if not os.path.exists(args.s):
     os.makedirs(args.s)
 
-BN = True
-ACTION_BITS = 13
-STATE_BITS = 50
-NUM_EPOCH = 30
-LR = 0.0001
-BATCH_SIZE = 128
+arg_dict = vars(args)
+for arg in arg_dict:
+    print(f"{arg}={arg_dict[arg]}", file=open(os.path.join(args.s, "args.txt"), "a"))
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+BN = True if args.bn == 1 else False
 
 encoder = torch.nn.Sequential(
     blocks.ConvBlock(in_channels=3, out_channels=64, kernel_size=4, stride=2, padding=1, batch_norm=BN),
     blocks.ConvBlock(in_channels=64, out_channels=128, kernel_size=4, stride=2, padding=1, batch_norm=BN),
     blocks.ConvBlock(in_channels=128, out_channels=256, kernel_size=4, stride=2, padding=1, batch_norm=BN),
     blocks.ConvBlock(in_channels=256, out_channels=512, kernel_size=4, stride=2, padding=1, batch_norm=BN),
-    blocks.ConvBlock(in_channels=512, out_channels=1024, kernel_size=4, stride=2, padding=1, batch_norm=BN),
+    blocks.ConvBlock(in_channels=512, out_channels=args.state_bits, kernel_size=4, stride=2, padding=1, batch_norm=BN),
     blocks.Avg([2, 3]),
-    blocks.MLP([1024, STATE_BITS]),
     blocks.GumbelSigmoidLayer(hard=False, T=1.0)
 )
 
 decoder = torch.nn.Sequential(
-    blocks.MLP([STATE_BITS+ACTION_BITS, 1024]),
-    blocks.Reshape([-1, 1024, 1, 1]),
-    blocks.ConvTransposeBlock(in_channels=1024, out_channels=512, kernel_size=4, stride=1, padding=0, batch_norm=BN),
+    # blocks.MLP([args.state_bits+args.action_bits, 512]),
+    blocks.Reshape([-1, args.state_bits+args.action_bits, 1, 1]),
+    blocks.ConvTransposeBlock(in_channels=args.state_bits+args.action_bits, out_channels=512, kernel_size=4, stride=1, padding=0, batch_norm=BN),
     blocks.ConvTransposeBlock(in_channels=512, out_channels=256, kernel_size=4, stride=2, padding=1, batch_norm=BN),
     blocks.ConvTransposeBlock(in_channels=256, out_channels=128, kernel_size=4, stride=2, padding=1, batch_norm=BN),
     blocks.ConvTransposeBlock(in_channels=128, out_channels=64, kernel_size=4, stride=2, padding=1, batch_norm=BN),
-    blocks.ConvTransposeBlock(in_channels=64, out_channels=32, kernel_size=4, stride=2, padding=1, batch_norm=BN),
-    torch.nn.ConvTranspose2d(in_channels=32, out_channels=3, kernel_size=4, stride=2, padding=1)
+    torch.nn.ConvTranspose2d(in_channels=64, out_channels=3, kernel_size=4, stride=2, padding=1)
 )
 
-encoder.to("cuda")
-decoder.to("cuda")
+encoder.to(device)
+decoder.to(device)
 
 model = DeepSymbolGenerator(encoder=encoder, decoder=decoder, subnetworks=[],
-                            device="cuda", lr=1e-4, path=args.s, coeff=9.0)
+                            device=device, lr=args.lr, path=args.s, coeff=81.0)
 model.print_model()
+encoder_param_count = get_parameter_count(model.encoder)
+decoder_param_count = get_parameter_count(model.decoder)
+print(f"Encoder params={encoder_param_count:,}")
+print(f"Decoder params={decoder_param_count:,}")
+print(f"Total={encoder_param_count+decoder_param_count:,}")
 
 # collect data
-for e in range(NUM_EPOCH):
-    subprocess.run(["python", "explore.py", "-N", "10000"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    data = StateActionDataset("./data")
-    loader = torch.utils.data.DataLoader(data, batch_size=BATCH_SIZE, num_workers=12)
+for e in range(args.e):
+    subprocess.run(["python", "explore_crafter.py", "-N", "1024", "-o", args.d])
+
+    if args.dt == 0:
+        data = StateActionDataset(args.d)
+    else:
+        data = StateActionEffectDataset(args.d)
+
+    loader = torch.utils.data.DataLoader(data, batch_size=args.bs, num_workers=12)
     model.train(1, loader)
