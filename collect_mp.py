6aad59630f35058aff229e0c0b204b6696862a2b
alper111 <ahmetoglu.alper@gmail.com>
2022-05-11 16:52:22 +0300

    number of processes

--- collect_mp.py
+++ collect_mp.py
@@ -12,17 +12,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser("Train DeepSym.")
     parser.add_argument("-d", help="data folder", type=str, required=True)
     parser.add_argument("-N", help="number of data per proc", type=int, required=True)
-    n_proc = 8
+    parser.add_argument("-p", help="number of procs", type=int, required=True)
     args = parser.parse_args()
     procs = []
     start = time.time()
-    for i in range(n_proc):
+    for i in range(args.p):
         p = Process(target=collect, args=[str(args.N), args.d, str(i*args.N)])
         p.start()
         procs.append(p)
 
-    for i in range(n_proc):
+    for i in range(args.p):
         procs[i].join()
     end = time.time()
     elapsed = end - start
-    print(f"Collected {n_proc*args.N} samples in {elapsed:.2f} seconds. {n_proc*args.N/elapsed}")
+    print(f"Collected {args.p*args.N} samples in {elapsed:.2f} seconds. {args.p*args.N/elapsed}")
