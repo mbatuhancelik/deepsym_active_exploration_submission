1a85575e0d2ce107867952cd55c392f68bfe65d1
alper111 <ahmetoglu.alper@gmail.com>
2022-05-31 10:29:56 +0300

    change channel num

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ for i, arg in enumerate(arg_dict):
 BN = True if args.bn == 1 else False
 
 encoder = torch.nn.Sequential(
-    blocks.ConvBlock(in_channels=4, out_channels=64, kernel_size=4, stride=2, padding=1, batch_norm=BN),
+    blocks.ConvBlock(in_channels=1, out_channels=64, kernel_size=4, stride=2, padding=1, batch_norm=BN),
     blocks.ConvBlock(in_channels=64, out_channels=128, kernel_size=4, stride=2, padding=1, batch_norm=BN),
     blocks.ConvBlock(in_channels=128, out_channels=256, kernel_size=4, stride=2, padding=1, batch_norm=BN),
     blocks.ConvBlock(in_channels=256, out_channels=512, kernel_size=4, stride=2, padding=1, batch_norm=BN),
