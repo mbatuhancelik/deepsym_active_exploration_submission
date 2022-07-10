03517c29fac9fc46e0f8ea0f0a37d334913946af
alper111 <ahmetoglu.alper@gmail.com>
2022-07-10 09:46:19 +0300

    update test script to current env

--- test.py
+++ test.py
@@ -10,14 +10,14 @@ from dataset import preprocess
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 STATE_BITS = 8
-ACTION_BITS = 16
+ACTION_BITS = 12
 BN = True
-EFF_MU = torch.tensor([-0.0003,  0.0002,  0.0004])
-EFF_STD = torch.tensor([0.0687, 0.0937, 0.0164])
+EFF_MU = torch.tensor([8.0498e-03,  2.4965e-04,  4.6637e-04])
+EFF_STD = torch.tensor([0.1055, 0.1474, 0.0296])
 
 
 encoder = torch.nn.Sequential(
-    blocks.ConvBlock(in_channels=3, out_channels=64, kernel_size=4, stride=2, padding=1, batch_norm=BN),
+    blocks.ConvBlock(in_channels=1, out_channels=64, kernel_size=4, stride=2, padding=1, batch_norm=BN),
     blocks.ConvBlock(in_channels=64, out_channels=128, kernel_size=4, stride=2, padding=1, batch_norm=BN),
     blocks.ConvBlock(in_channels=128, out_channels=256, kernel_size=4, stride=2, padding=1, batch_norm=BN),
     blocks.ConvBlock(in_channels=256, out_channels=512, kernel_size=4, stride=2, padding=1, batch_norm=BN),
@@ -26,17 +26,19 @@ encoder = torch.nn.Sequential(
     blocks.GumbelSigmoidLayer(hard=False, T=1.0)
 )
 
-encoder_layer = torch.nn.TransformerEncoderLayer(d_model=STATE_BITS+ACTION_BITS, nhead=1, batch_first=True)
+projector = torch.nn.Linear(STATE_BITS+ACTION_BITS, 128)
+encoder_layer = torch.nn.TransformerEncoderLayer(d_model=128, nhead=8, batch_first=True)
 decoder_att = torch.nn.TransformerEncoder(encoder_layer, num_layers=4)
-decoder = blocks.MLP([STATE_BITS+ACTION_BITS, 256, 256, 256, 3], batch_norm=BN)
+decoder = blocks.MLP([128, 256, 256, 256, 3], batch_norm=BN)
 
 encoder.to(device)
 decoder.to(device)
+projector.to(device)
 decoder_att.to(device)
 
-model = DeepSymv3(encoder=encoder, decoder=decoder, decoder_att=decoder_att,
-                  device=device, lr=0.0001, path="save/3x5_t4_4b_8/", coeff=1.0)
-model.load("_best")
+model = DeepSymv3(encoder=encoder, decoder=decoder, decoder_att=decoder_att, projector=projector,
+                  device=device, lr=0.0001, path="save/v4_1/", coeff=1.0)
+model.load("_last")
 model.eval_mode()
 
 model.print_model()
@@ -45,16 +47,16 @@ for name in model.module_names:
     for param in getattr(model, name).parameters():
         param.requires_grad = False
 
-valid_objects = {i: True for i in range(4, 13)}
-env = environment.BlocksWorld_v2(gui=1, min_objects=5, max_objects=9)
+valid_objects = {i: True for i in range(4, 7)}
+env = environment.BlocksWorld_v2(gui=1, min_objects=3, max_objects=3)
 env._step(10000)
 
 while True:
     _, depth_img, seg = env.state()
-    depth_img = ((depth_img - depth_img.min()) / (depth_img.max() - depth_img.min()))*255
+    depth_img = utils.normalize_depth_img(depth_img)
     depth_img = torch.tensor(depth_img).unsqueeze(0)
     seg = torch.tensor(seg)
-    padded, pad_mask = preprocess(depth_img, seg, valid_objects, env.num_objects)
+    padded, pad_mask = preprocess(depth_img, seg, valid_objects, env.num_objects, aug=False, old=True)
     n = padded.shape[0]
     action = input()
     if action == "reset":
@@ -69,14 +71,15 @@ while True:
         break
     else:
         action = torch.tensor([int(a) for a in action], dtype=torch.long)
+        eye = torch.eye(6)
         sample = {
             "state": padded.unsqueeze(0),
-            "action": action.unsqueeze(0),
+            "action": torch.cat([eye[action[0]], eye[action[1]]], dim=-1).reshape(1, 1, 12).repeat(1, 3, 1),
             "pad_mask": pad_mask.unsqueeze(0)
         }
         pred_z, pred_e = model.forward(sample, eval_mode=True)
         pred_e = pred_e*EFF_STD + EFF_MU
-        effect = env.step(action[:2].tolist(), action[2:].tolist())
+        effect = env.step(action[0].item(), action[1].item())
         print("Predicted // Actual")
         print("------------------")
         for p_e, a_e in zip(pred_e[0], effect):
