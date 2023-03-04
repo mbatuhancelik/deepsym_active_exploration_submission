de899a1df7be71fe2301a54273b4e4942939aba1
Batuhan <batuhancelik.boun@gmail.com>
2023-03-04 12:24:22 +0300

    merging parallel exploration data

---
+++ merge_data.py
@@ -0,0 +1,32 @@
+import time
+import os
+import argparse
+
+import torch
+import numpy as np
+
+
+parser = argparse.ArgumentParser("Merge exploration data.")
+parser.add_argument("-o", help="output folder", type=str, required=True)
+parser.add_argument("-i", help="number of rolls", type=int, required=True)
+args = parser.parse_args()
+
+keys = ["action", "effect", "mask", "state"]
+
+data = {}
+for key in keys:
+    data[key] = torch.load(f"./data/v5/{key}_0.pt")
+
+
+for i in range(1,args.i):
+    for key in keys:
+        dk = torch.load(f"./data/v5/{key}_{i}.pt")
+        data[key] = torch.cat([data[key], dk], axis = 0)
+
+for key in keys:
+    torch.save(data[key], f"./data/v5/{key}.pt")
+
+for i in range(args.i):
+    for key in keys:
+        os.remove(f"./data/v5/{key}_{i}.pt")
+        
