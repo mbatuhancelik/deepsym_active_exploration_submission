3ecdf01f31d7da8e84b99ff047ec58afe94f0804
Batuhan <batuhancelik.boun@gmail.com>
2023-03-08 12:16:05 +0300

    now uses -o argument

--- merge_data.py
+++ merge_data.py
@@ -15,18 +15,18 @@ keys = ["action", "effect", "mask", "state"]
 
 data = {}
 for key in keys:
-    data[key] = torch.load(f"./data/v5/{key}_0.pt")
+    data[key] = torch.load(f"{args.o}/{key}_0.pt")
 
 
 for i in range(1,args.i):
     for key in keys:
-        dk = torch.load(f"./data/v5/{key}_{i}.pt")
+        dk = torch.load(f"{args.o}/{key}_{i}.pt")
         data[key] = torch.cat([data[key], dk], axis = 0)
 
 for key in keys:
-    torch.save(data[key], f"./data/v5/{key}.pt")
+    torch.save(data[key], f"{args.o}/{key}.pt")
 
 for i in range(args.i):
     for key in keys:
-        os.remove(f"./data/v5/{key}_{i}.pt")
+        os.remove(f"{args.o}/{key}_{i}.pt")
         
