59f6caf8f337b0279e4c12bce91957271d4459fe
alper111 <ahmetoglu.alper@gmail.com>
2023-04-09 21:02:08 +0300

    cleanup merge data

--- merge_data.py
+++ merge_data.py
@@ -1,9 +1,7 @@
-import time
 import os
 import argparse
 
 import torch
-import numpy as np
 
 
 parser = argparse.ArgumentParser("Merge exploration data.")
@@ -13,20 +11,8 @@ args = parser.parse_args()
 
 keys = ["action", "effect", "mask", "state"]
 
-data = {}
 for key in keys:
-    data[key] = torch.load(f"{args.o}/{key}_0.pt")
-
-
-for i in range(1,args.i):
-    for key in keys:
-        dk = torch.load(f"{args.o}/{key}_{i}.pt")
-        data[key] = torch.cat([data[key], dk], axis = 0)
-
-for key in keys:
-    torch.save(data[key], f"{args.o}/{key}.pt")
-
-for i in range(args.i):
-    for key in keys:
-        os.remove(f"{args.o}/{key}_{i}.pt")
-        
+    field = torch.cat([torch.load(os.path.join(args.o, f"{key}_{i}.pt")) for i in range(args.i)], dim=0)
+    torch.save(field, os.path.join(os.path.join(args.o, f"{key}.pt")))
+    for i in range(args.i):
+        os.remove(os.path.join(args.o, f"{key}_{i}.pt"))
