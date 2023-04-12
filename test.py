9f15180adb43db07a278591081e218170f5ee215
alper111 <ahmetoglu.alper@gmail.com>
2023-04-12 16:27:47 +0300

    sync test for the blocksworld env

--- test.py
+++ test.py
@@ -1,94 +1,127 @@
+import sys
+
 import torch
+import wandb
 
-from models import DeepSymv3
-import blocks
-import utils
 import environment
-from dataset import preprocess
+import utils
 
 
-device = utils.get_device()
+def draw_action(env):
+    text_items = {}
+    for o_id in env.obj_dict:
+        text_items[o_id] = env._p.addUserDebugText(str(o_id), [0., 0., 0.05],
+                                                   textColorRGB=[1.0, 0.0, 0.0],
+                                                   parentObjectUniqueId=env.obj_dict[o_id],
+                                                   textSize=1.5)
 
-STATE_BITS = 8
-ACTION_BITS = 12
-BN = True
-EFF_MU = torch.tensor([8.0498e-03,  2.4965e-04,  4.6637e-04])
-EFF_STD = torch.tensor([0.1055, 0.1474, 0.0296])
+    from_obj = int(input("From: "))
+    text_items[from_obj] = env._p.addUserDebugText(str(from_obj), [0., 0., 0.05],
+                                                   textColorRGB=[0.0, 0.0, 1.0],
+                                                   parentObjectUniqueId=env.obj_dict[from_obj],
+                                                   textSize=1.5,
+                                                   replaceItemUniqueId=text_items[from_obj])
+    x_debug_texts = []
+    for i in [-1, 0, 1]:
+        for j in [-1, 0, 1]:
+            pos = [env.ds * i, env.ds * j, 0.01]
+            x_debug_texts.append(env._p.addUserDebugText("*", pos,
+                                                         textColorRGB=[0.0, 1.0, 1.0],
+                                                         textSize=1.5,
+                                                         parentObjectUniqueId=env.obj_dict[from_obj]))
+    dxdy = input("From dxdy: ")
+    dxdy = dxdy.split(" ")
+    from_dx = int(dxdy[0])
+    from_dy = int(dxdy[1])
+    for debug_text in x_debug_texts:
+        env._p.removeUserDebugItem(debug_text)
+    x_debug_text = env._p.addUserDebugText("*", [env.ds * from_dx, env.ds * from_dy, 0.01],
+                                           textColorRGB=[0.0, 1.0, 1.0],
+                                           textSize=1.5,
+                                           parentObjectUniqueId=env.obj_dict[from_obj])
+
+    to_obj = int(input("To: "))
+    text_items[to_obj] = env._p.addUserDebugText(str(to_obj), [0., 0., 0.05],
+                                                 textColorRGB=[0.0, 0.0, 1.0],
+                                                 parentObjectUniqueId=env.obj_dict[to_obj],
+                                                 textSize=1.5,
+                                                 replaceItemUniqueId=text_items[to_obj])
+    y_debug_texts = []
+    for i in [-1, 0, 1]:
+        for j in [-1, 0, 1]:
+            pos = [env.ds * i, env.ds * j, 0.01]
+            y_debug_texts.append(env._p.addUserDebugText("*", pos,
+                                                         textColorRGB=[0.0, 1.0, 1.0],
+                                                         textSize=1.5,
+                                                         parentObjectUniqueId=env.obj_dict[to_obj]))
+    dxdy = input("To dxdy: ")
+    dxdy = dxdy.split(" ")
+    to_dx = int(dxdy[0])
+    to_dy = int(dxdy[1])
+    for debug_text in y_debug_texts:
+        env._p.removeUserDebugItem(debug_text)
+    y_debug_text = env._p.addUserDebugText("*", [env.ds * to_dx, env.ds * to_dy, 0.01],
+                                           textColorRGB=[0.0, 1.0, 1.0],
+                                           textSize=1.5,
+                                           parentObjectUniqueId=env.obj_dict[to_obj])
+
+    init_rot, final_rot = input("Init rot, final rot: ").split(" ")
+    init_rot = int(init_rot)
+    final_rot = int(final_rot)
+    action = [from_obj, to_obj, from_dx, from_dy, to_dx, to_dy, init_rot, final_rot]
+
+    text_items[from_obj] = env._p.addUserDebugText(str(from_obj), [0., 0., 0.05],
+                                                   textColorRGB=[1.0, 0.0, 0.0],
+                                                   parentObjectUniqueId=env.obj_dict[from_obj],
+                                                   textSize=1.5,
+                                                   replaceItemUniqueId=text_items[from_obj])
+    text_items[to_obj] = env._p.addUserDebugText(str(to_obj), [0., 0., 0.05],
+                                                 textColorRGB=[1.0, 0.0, 0.0],
+                                                 parentObjectUniqueId=env.obj_dict[to_obj],
+                                                 textSize=1.5,
+                                                 replaceItemUniqueId=text_items[to_obj])
+
+    return action, [x_debug_text, y_debug_text]
 
 
-encoder = torch.nn.Sequential(
-    blocks.ConvBlock(in_channels=3, out_channels=64, kernel_size=4, stride=2, padding=1, batch_norm=BN),
-    blocks.ConvBlock(in_channels=64, out_channels=128, kernel_size=4, stride=2, padding=1, batch_norm=BN),
-    blocks.ConvBlock(in_channels=128, out_channels=256, kernel_size=4, stride=2, padding=1, batch_norm=BN),
-    blocks.ConvBlock(in_channels=256, out_channels=512, kernel_size=4, stride=2, padding=1, batch_norm=BN),
-    blocks.Avg([2, 3]),
-    blocks.MLP([512, STATE_BITS]),
-    blocks.GumbelSigmoidLayer(hard=False, T=1.0)
-)
-
-projector = torch.nn.Linear(STATE_BITS+ACTION_BITS, 128)
-encoder_layer = torch.nn.TransformerEncoderLayer(d_model=128, nhead=8, batch_first=True)
-decoder_att = torch.nn.TransformerEncoder(encoder_layer, num_layers=4)
-decoder = blocks.MLP([128, 256, 256, 256, 3], batch_norm=BN)
-
-encoder.to(device)
-decoder.to(device)
-projector.to(device)
-decoder_att.to(device)
-
-model = DeepSymv3(encoder=encoder, decoder=decoder, decoder_att=decoder_att, projector=projector,
-                  device=device, lr=0.0001, path="save/v4_varying_new/", coeff=1.0)
-model.load("_best")
-model.eval_mode()
-
+run_id = sys.argv[1]
+run = wandb.init(entity="colorslab", project="multideepsym", resume="must", id=run_id)
+run.config["device"] = "cpu"
+model = utils.create_model_from_config(run.config)
+model.load("_best", from_wandb=True)
 model.print_model()
-for name in model.module_names:
-    print(f"{name} params={utils.get_parameter_count(getattr(model, name)):,}")
-    for param in getattr(model, name).parameters():
-        param.requires_grad = False
 
-valid_objects = {i: True for i in range(4, 7)}
-env = environment.BlocksWorld_v2(gui=1, min_objects=1, max_objects=3)
-env._step(10000)
+for name in model.module_names:
+    getattr(model, name).eval()
+
+
+env = environment.BlocksWorld_v4(gui=1, min_objects=8, max_objects=13)
 eff_arrow_ids = []
 while True:
     while len(eff_arrow_ids) > 0:
         arrow_id = eff_arrow_ids.pop()
         env._p.removeBody(arrow_id)
-    _, depth_img, seg = env.state()
-    depth_img = utils.normalize_depth_img(depth_img)
-    depth_img = torch.tensor(depth_img).unsqueeze(0)
-    seg = torch.tensor(seg)
-    padded, pad_mask = preprocess(depth_img, seg, valid_objects, env.num_objects, aug=False, old=False)
-    n = padded.shape[0]
-    action = input()
-    if action == "reset":
-        env.reset_objects()
-        continue
-    elif action[:4] == "wait":
-        steps = int(action[4:].rstrip())
-        env._step(steps)
-        continue
-    elif action == "exit":
-        del env
-        break
-    else:
-        action = torch.tensor([int(a) for a in action], dtype=torch.long)
-        eye = torch.eye(6)
-        sample = {
-            "state": padded.unsqueeze(0),
-            "action": torch.cat([eye[action[0]], eye[action[1]]], dim=-1)
-            .reshape(1, 1, 12).repeat(1, padded.shape[0], 1),
-            "pad_mask": pad_mask.unsqueeze(0)
-        }
-        pred_z, pred_e = model.forward(sample, eval_mode=True)
-        pred_e = pred_e*EFF_STD + EFF_MU
-        poses = env.state_obj_poses()
-        for o_i, e_i in zip(poses, pred_e[0]):
-            eff_arrow_ids.append(utils.create_arrow(env._p, o_i[:3], o_i[:3]+e_i.numpy()))
-        effect = env.step(action[0].item(), action[1].item())
-        print("Predicted // Actual")
-        print("------------------")
-        for p_e, a_e, z in zip(pred_e[0], effect, pred_z[0]):
-            print(f"{p_e[0]:.3f}, {p_e[1]:.3f}, {p_e[2]:.3f} // {a_e[0]:.3f}, {a_e[1]:.3f}, {a_e[2]:.3f}"
-                  f"-- {z[0]}, {z[1]}, {z[2]}, {z[3]}, {z[4]}, {z[5]}, {z[6]}, {z[7]}")
+    state, types = env.state()
+    state = torch.tensor(state)
+    types = torch.tensor(types).reshape(-1, 1)
+    state = torch.cat([torch.tensor(state), torch.tensor(types)], dim=-1)
+    action, debug_texts = draw_action(env)
+    action_vector = torch.zeros(state.shape[0], 4, dtype=torch.float)
+    action_vector[action[0]] = torch.tensor([-1, action[2], action[3], action[6]], dtype=torch.float)
+    action_vector[action[1]] = torch.tensor([1, action[4], action[5], action[7]], dtype=torch.float)
+    sample = {"state": state.unsqueeze(0), "action": action_vector.unsqueeze(0), "pad_mask": torch.ones(1, state.shape[0])}
+    _, e_pred = model.forward(sample, eval_mode=True)
+    e_pred = e_pred.detach()
+    e_pred = e_pred.reshape(-1, 2, 9)
+    for e_i, s_i in zip(e_pred, state):
+        e_before, e_after = e_i
+        from_before = s_i[:3].clone()
+        from_after = s_i[:3].clone() + e_before[:3].clone()
+        to_before = s_i[:3].clone()
+        to_before[2] = 0.9
+        to_after = to_before.clone() + e_after[:3].clone()
+        eff_arrow_ids.append(utils.create_arrow(env._p, from_before, from_after, color=[1.0, 0.0, 0.0, 0.75]))
+        eff_arrow_ids.append(utils.create_arrow(env._p, to_before, to_after, color=[0.0, 1.0, 0.0, 0.75]))
+    env.step(*action)
+    for debug_text in debug_texts:
+        env._p.removeUserDebugItem(debug_text)
