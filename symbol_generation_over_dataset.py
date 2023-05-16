427b576b710419a620fad47bd64643f25e2ad4b0
alper111 <ahmetoglu.alper@gmail.com>
2023-05-16 14:52:54 +0300

    update symbol generation w.r.t. v2-fix

--- symbol_generation_over_dataset.py
+++ symbol_generation_over_dataset.py
@@ -1,68 +1,63 @@
+import os
+import sys
+
 import wandb
-import argparse
 import torch
 import utils
 from dataset import StateActionEffectDataset
 
 
-
-parser = argparse.ArgumentParser("Generate Z and E values for the dataset.")
-parser.add_argument("-id", help="experiment id")
-parser.add_argument("-db", help="dataset folder name under ./data")
-args = parser.parse_args()
-
-
-run = wandb.init(entity="colorslab",project="multideepsym", resume="must", id=args.id)
-run = wandb.Api().run("colorslab/multideepsym/" + args.id)
-run.config["device"] = "cpu"
-
-
+run_id = sys.argv[1]
+run = wandb.init(entity="colorslab",project="multideepsym", resume="must", id=run_id)
 model = utils.create_model_from_config(run.config)
 model.load("_best", from_wandb=True)
+model.print_model()
 
-dataset = StateActionEffectDataset(args.db, split="test")
- 
+for name in model.module_names:
+    getattr(model, name).eval()
 
-state_pad = torch.zeros(size=(1,13,1), device='cpu')
-Z = []
-E = []
-Z_prime = []
-for i in range(len(dataset)):
+
+dataset = StateActionEffectDataset(run.config["dataset_name"], split="val")
+loader = torch.utils.data.DataLoader(dataset, batch_size=run.config["batch_size"])
+
+z_obj_pre = []
+z_rel_pre = []
+z_act = []
+z_obj_post = []
+z_rel_post = []
+mask = []
+for sample in loader:
     with torch.no_grad():
-        data = dataset[i]
-        for key in ["action", "state", "effect"]:
-            data[key] = data[key].unsqueeze(0).expand(-1, 13, -1)
+        z_i = model.encode(sample["state"], eval_mode=True)
+        zr_i = model.attn_weights(sample["state"], sample["pad_mask"], eval_mode=True)
+        z_f = model.encode(sample["post_state"], eval_mode=True)
+        zr_f = model.attn_weights(sample["post_state"], sample["pad_mask"], eval_mode=True)
+        z_obj_pre.append(z_i.cpu())
+        z_rel_pre.append(zr_i.cpu())
+        z_act.append(sample["action"].cpu())
+        z_obj_post.append(z_f.cpu())
+        z_rel_post.append(zr_f.cpu())
+        mask.append(sample["pad_mask"].cpu())
 
-        data["pad_mask"] = data["pad_mask"].unsqueeze(0).expand(-1, 13)
+z_obj_pre = torch.cat(z_obj_pre, axis = 0)
+z_rel_pre = torch.cat(z_rel_pre, axis = 0)
+z_act = torch.cat(z_act, axis = 0)
+z_obj_post = torch.cat(z_obj_post, axis = 0)
+z_rel_post = torch.cat(z_rel_post, axis = 0)
+mask = torch.cat(mask, axis = 0)
 
-        z , e = model.forward(data, eval_mode=True)
-        data["state"] += torch.cat([e, state_pad],dim=-1).to('cpu')
-        z_prime , _ = model.forward(data,eval_mode= True)
-        Z.append(z.to("cpu"))
-        E.append(e.to("cpu"))
-        Z_prime.append(z_prime.to("cpu"))
-        if(i %1000 == 0):
-            print(i, "processed")
+torch.save(z_obj_pre, os.path.join(run.config["save_folder"], "z_obj_pre.pt"))
+torch.save(z_rel_pre, os.path.join(run.config["save_folder"], "z_rel_pre.pt"))
+torch.save(z_act, os.path.join(run.config["save_folder"], "z_act.pt"))
+torch.save(z_obj_post, os.path.join(run.config["save_folder"], "z_obj_post.pt"))
+torch.save(z_rel_post, os.path.join(run.config["save_folder"], "z_rel_post.pt"))
+torch.save(mask, os.path.join(run.config["save_folder"], "mask.pt"))
 
-Z = torch.cat(Z, axis = 0)
-Z_prime = torch.cat(Z_prime, axis = 0)
-E = torch.cat(E, axis = 0)
+wandb.save(os.path.join(run.config["save_folder"], "z_obj_pre.pt"))
+wandb.save(os.path.join(run.config["save_folder"], "z_rel_pre.pt"))
+wandb.save(os.path.join(run.config["save_folder"], "z_act.pt"))
+wandb.save(os.path.join(run.config["save_folder"], "z_obj_post.pt"))
+wandb.save(os.path.join(run.config["save_folder"], "z_rel_post.pt"))
+wandb.save(os.path.join(run.config["save_folder"], "mask.pt"))
 
-folder = "./wandb/latest-run/files/save/deneme/"
-
-torch.save(Z, folder + "Z.pt")
-torch.save(Z_prime, folder + "Z_prime.pt")
-torch.save(E, folder + "E.pt")
-
-wandb.save(folder + "Z.pt")
-wandb.save(folder + "Z_prime.pt")
-wandb.save(folder + "E.pt")
-
-
-
-        
-# run = wandb.init(id="e16396qx")
-# artifact = run.use_artifact('colorslab/multideepsym/v4:v0', type='dataset')
-# artifact_dir = artifact.download()
-# run = wandb.Api().run("colorslab/multideepsym/e16396qx")
-
+run.finish()
