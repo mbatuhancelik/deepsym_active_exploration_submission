f3f343f40319d3cd142cb7ef77a57a6e6d5a8702
alper111 <ahmetoglu.alper@gmail.com>
2023-05-23 10:37:44 +0300

    re-organize symbol forward/generation

--- symbol_generation_over_dataset.py
+++ symbol_generation_over_dataset.py
@@ -7,6 +7,51 @@ import utils
 from dataset import StateActionEffectDataset
 
 
+def encode_dataset(loader, model):
+    z_obj_pre = []
+    z_rel_pre = []
+    z_act = []
+    z_obj_post = []
+    z_rel_post = []
+    mask = []
+    for sample in loader:
+        with torch.no_grad():
+            z_i = model.encode(sample["state"], eval_mode=True)
+            zr_i = model.attn_weights(sample["state"], sample["pad_mask"], eval_mode=True)
+            z_f = model.encode(sample["post_state"], eval_mode=True)
+            zr_f = model.attn_weights(sample["post_state"], sample["pad_mask"], eval_mode=True)
+            z_obj_pre.append(z_i.cpu())
+            z_rel_pre.append(zr_i.cpu())
+            z_act.append(sample["action"].cpu())
+            z_obj_post.append(z_f.cpu())
+            z_rel_post.append(zr_f.cpu())
+            mask.append(sample["pad_mask"].cpu())
+
+    z_obj_pre = torch.cat(z_obj_pre, axis=0)
+    z_rel_pre = torch.cat(z_rel_pre, axis=0)
+    z_act = torch.cat(z_act, axis=0)
+    z_obj_post = torch.cat(z_obj_post, axis=0)
+    z_rel_post = torch.cat(z_rel_post, axis=0)
+    mask = torch.cat(mask, axis=0)
+    return z_obj_pre, z_rel_pre, z_act, z_obj_post, z_rel_post, mask
+
+
+def save_and_upload(z_obj_pre, z_rel_pre, z_act, z_obj_post, z_rel_post, mask, run, name):
+    torch.save(z_obj_pre, os.path.join(run.config["save_folder"], f"{name}_z_obj_pre.pt"))
+    torch.save(z_rel_pre, os.path.join(run.config["save_folder"], f"{name}_z_rel_pre.pt"))
+    torch.save(z_act, os.path.join(run.config["save_folder"], f"{name}_z_act.pt"))
+    torch.save(z_obj_post, os.path.join(run.config["save_folder"], f"{name}_z_obj_post.pt"))
+    torch.save(z_rel_post, os.path.join(run.config["save_folder"], f"{name}_z_rel_post.pt"))
+    torch.save(mask, os.path.join(run.config["save_folder"], f"{name}_mask.pt"))
+
+    wandb.save(os.path.join(run.config["save_folder"], f"{name}_z_obj_pre.pt"))
+    wandb.save(os.path.join(run.config["save_folder"], f"{name}_z_rel_pre.pt"))
+    wandb.save(os.path.join(run.config["save_folder"], f"{name}_z_act.pt"))
+    wandb.save(os.path.join(run.config["save_folder"], f"{name}_z_obj_post.pt"))
+    wandb.save(os.path.join(run.config["save_folder"], f"{name}_z_rel_post.pt"))
+    wandb.save(os.path.join(run.config["save_folder"], f"{name}_mask.pt"))
+
+
 run_id = sys.argv[1]
 run = wandb.init(entity="colorslab", project="multideepsym", resume="must", id=run_id)
 model = utils.create_model_from_config(run.config)
@@ -17,50 +62,18 @@ for name in model.module_names:
     getattr(model, name).eval()
 
 
-dataset = StateActionEffectDataset(run.config["dataset_name"], split="val")
-loader = torch.utils.data.DataLoader(dataset, batch_size=run.config["batch_size"])
-
-z_obj_pre = []
-z_rel_pre = []
-z_act = []
-z_obj_post = []
-z_rel_post = []
-mask = []
-for sample in loader:
-    with torch.no_grad():
-        z_i = model.encode(sample["state"], eval_mode=True)
-        zr_i = model.attn_weights(sample["state"], sample["pad_mask"], eval_mode=True)
-        z_f = model.encode(sample["post_state"], eval_mode=True)
-        zr_f = model.attn_weights(sample["post_state"], sample["pad_mask"], eval_mode=True)
-        z_obj_pre.append(z_i.cpu())
-        z_rel_pre.append(zr_i.cpu())
-        z_act.append(sample["action"].cpu())
-        z_obj_post.append(z_f.cpu())
-        z_rel_post.append(zr_f.cpu())
-        mask.append(sample["pad_mask"].cpu())
-
-z_obj_pre = torch.cat(z_obj_pre, axis=0)
-z_rel_pre = torch.cat(z_rel_pre, axis=0)
-z_act = torch.cat(z_act, axis=0)
-z_obj_post = torch.cat(z_obj_post, axis=0)
-z_rel_post = torch.cat(z_rel_post, axis=0)
-mask = torch.cat(mask, axis=0)
+train_set = StateActionEffectDataset(run.config["dataset_name"], split="train")
+val_set = StateActionEffectDataset(run.config["dataset_name"], split="val")
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=run.config["batch_size"])
+val_loader = torch.utils.data.DataLoader(val_set, batch_size=run.config["batch_size"])
 
 if not os.path.exists(run.config["save_folder"]):
     os.makedirs(run.config["save_folder"])
 
-torch.save(z_obj_pre, os.path.join(run.config["save_folder"], "z_obj_pre.pt"))
-torch.save(z_rel_pre, os.path.join(run.config["save_folder"], "z_rel_pre.pt"))
-torch.save(z_act, os.path.join(run.config["save_folder"], "z_act.pt"))
-torch.save(z_obj_post, os.path.join(run.config["save_folder"], "z_obj_post.pt"))
-torch.save(z_rel_post, os.path.join(run.config["save_folder"], "z_rel_post.pt"))
-torch.save(mask, os.path.join(run.config["save_folder"], "mask.pt"))
+z_obj_pre, z_rel_pre, z_act, z_obj_post, z_rel_post, mask = encode_dataset(train_loader, model)
+save_and_upload(z_obj_pre, z_rel_pre, z_act, z_obj_post, z_rel_post, mask, run, "train")
 
-wandb.save(os.path.join(run.config["save_folder"], "z_obj_pre.pt"))
-wandb.save(os.path.join(run.config["save_folder"], "z_rel_pre.pt"))
-wandb.save(os.path.join(run.config["save_folder"], "z_act.pt"))
-wandb.save(os.path.join(run.config["save_folder"], "z_obj_post.pt"))
-wandb.save(os.path.join(run.config["save_folder"], "z_rel_post.pt"))
-wandb.save(os.path.join(run.config["save_folder"], "mask.pt"))
+z_obj_pre, z_rel_pre, z_act, z_obj_post, z_rel_post, mask = encode_dataset(val_loader, model)
+save_and_upload(z_obj_pre, z_rel_pre, z_act, z_obj_post, z_rel_post, mask, run, "val")
 
 run.finish()
