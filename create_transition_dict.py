c717e8f91c6aa81a86fa8b5aff6b02772bcefbef
alper111 <ahmetoglu.alper@gmail.com>
2023-06-23 08:36:30 +0300

    don't add permutations, permute at test time

--- create_transition_dict.py
+++ create_transition_dict.py
@@ -25,6 +25,7 @@ dictionary = {}
 for i, (obj_pre, rel_pre, action, obj_post, rel_post, mask) in enumerate(loader):
     valid_objects = torch.where(mask > 0)[1].tolist()
     permuted_ary = utils.permute(valid_objects)
+    key_found = False
     for permutation in permuted_ary:
         permuted = utils.permute_symbols(obj_pre[0], rel_pre[0], action[0],
                                          obj_post[0], rel_post[0], permutation)
@@ -32,17 +33,38 @@ for i, (obj_pre, rel_pre, action, obj_post, rel_post, mask) in enumerate(loader)
         key = (obj_pre_p, rel_pre_p)
         value = (obj_post_p, rel_post_p)
         if key in dictionary:
-            if action_p in dictionary[key]:
-                if value in dictionary[key][action_p]:
-                    dictionary[key][action_p][value] += 1
-                else:
-                    dictionary[key][action_p][value] = 1
+            key_found = True
+            break
+
+    # if a permutation is not found, use the original order
+    if not key_found:
+        obj_pre_p = tuple(utils.binary_tensor_to_str(obj_pre[0, valid_objects]))
+        rel_pre_p = []
+        for r in rel_pre[0]:
+            rel_pre_p.append(tuple(utils.binary_tensor_to_str(r[:len(valid_objects), :len(valid_objects)])))
+        rel_pre_p = tuple(rel_pre_p)
+        action_p = tuple(utils.binary_tensor_to_str(action[0, valid_objects]))
+        obj_post_p = tuple(utils.binary_tensor_to_str(obj_post[0, valid_objects]))
+        rel_post_p = []
+        for r in rel_post[0]:
+            rel_post_p.append(tuple(utils.binary_tensor_to_str(r[:len(valid_objects), :len(valid_objects)])))
+        rel_post_p = tuple(rel_post_p)
+        key = (obj_pre_p, rel_pre_p)
+        value = (obj_post_p, rel_post_p)
+
+    if key in dictionary:
+        if action_p in dictionary[key]:
+            if value in dictionary[key][action_p]:
+                dictionary[key][action_p][value] += 1
             else:
-                dictionary[key][action_p] = {value: 1}
+                dictionary[key][action_p][value] = 1
         else:
-            dictionary[key] = {action_p: {value: 1}}
+            dictionary[key][action_p] = {value: 1}
+    else:
+        dictionary[key] = {action_p: {value: 1}}
+
     if i % 100 == 0:
-        print(f"Processed {i} / {len(loader)}")
+        print(f"Processed {i} / {len(loader)}, dict size: {len(dictionary)}")
 
 if not os.path.exists(run.config["save_folder"]):
     os.makedirs(run.config["save_folder"])
