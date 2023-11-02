bb3082022b164c9436d3ab00aef4ba09f0e10cd9
batuhan celik <batuhancelik.boun@gmail.com>
2023-11-02 20:49:02 +0300

    implement council search w/o future sight

---
+++ council_manager.py
@@ -0,0 +1,21 @@
+import os
+import torch
+
+PATH="./council"
+
+def load(path=PATH):
+    council_list = os.listdir("./council")
+    assert(len(council_list)> 2)
+    council = []
+    for m in council_list:
+        council.append(torch.load(f"./council/{m}"))
+        council[-1].to("cpu")
+    return council
+def save(council ,path=PATH):
+    for i, m in enumerate(council):
+        torch.save(m, f"{PATH}/{i}.pt")
+
+
+c = load()
+save(c)
+        
