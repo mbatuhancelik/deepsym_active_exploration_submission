113b68bd84ba24b0ed5b5a57b9dea892a15f6aa1
batuhan celik <batuhancelik.boun@gmail.com>
2024-01-12 10:53:06 +0300

    collect only non existing data

--- scripts/collect_active.sh
+++ scripts/collect_active.sh
@@ -9,15 +9,22 @@ N=600
 pids=()
 echo "collecting dataset $1 "
 # Start processes in the background
-mkdir ./data/$1
+# mkdir ./data/$1
 for ((i=0; i<num_data_collectors; i++)); do
+  if [ ! -e "./data/$1/state_$i.pt" ]; 
+  then
+  echo "File '$i' created."
   CUDA_VISIBLE_DEVICES=0 nohup python explore_council.py -N $N -T 10 -i $i -o ./data/$1  -gen $3 &
   pids+=($!)  # Store the PID of the background process
+  fi
 done
 
 for ((i=0; i<num_data_collectors; i++)); do
+  if [ ! -e "./data/$1/state_$i.pt" ]; 
+  then
   CUDA_VISIBLE_DEVICES=1 nohup python explore_council.py -N $N -T 10 -i $((i + num_data_collectors)) -o ./data/$1 -gen $3 &
   pids+=($!)  # Store the PID of the background process
+  fi
 done
 
 # Wait for all processes to finish
